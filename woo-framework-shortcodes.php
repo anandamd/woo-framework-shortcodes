85e5b284fcd2e147344eb841471554fe3ac978e6
Anand Shah <anandamd@yahoo.co.in>
2016-01-30 11:53:01 +0530

    Fix class var issues

--- woo-framework-shortcodes.php
+++ woo-framework-shortcodes.php
@@ -1,6 +1,5 @@
 <?php
 if ( ! defined('ABSPATH') ) exit;
-
 /**
 * Plugin Name: Woo Framework Shortcodes
 * Plugin URI: https://github.com/anandamd/woo-framework-shortcodes
@@ -9,39 +8,31 @@ if ( ! defined('ABSPATH') ) exit;
 * License: GPLv2
 * Version: 1.0.0
 * */
-
 if ( ! class_exists( 'WF_Shortcodes' ) ) {
 	
 	class WF_Shortcodes {
-
-		$functions_path = '';
+		
+		protected $functions_path;		
 		
 		public function __construct() {
-			add_action( 'wp_head', array( $this, 'woothemes_wp_head' ), 10 );
 			
-			$functions_path = plugin_dir_path( __FILE__ ) . 'functions/';
+			add_action( 'wp_head', array( $this, 'wfs_wp_head' ), 10 );			
+
+			$this->functions_path = plugin_dir_path( __FILE__ ) . 'functions/';			
+			require_once ( $this->functions_path . 'admin-shortcodes.php' );	// Woo Shortcodes
+			
+			// Load certain files only in the WordPress admin.
+			if ( is_admin() ) {
+				require_once( $this->functions_path . 'admin-shortcode-generator.php' ); 	    // Framework Shortcode generator // 2011-01-21.
+			}
 		}
-		
-		require_once ( $functions_path . 'admin-shortcodes.php' );	// Woo Shortcodes
 
-
-		if ( ! function_exists( 'woothemes_wp_head' ) ) {
-			/**
-			 * Output the default WooFramework "head" markup in the "head" section.
-			 * @return void
-			 */
-			public function woothemes_wp_head() {
+		public function wfs_wp_head() {
 					
-				if ( function_exists( 'woo_shortcode_stylesheet' ) )
-					woo_shortcode_stylesheet();
+			if ( function_exists( 'woo_shortcode_stylesheet' ) ) 
+				woo_shortcode_stylesheet();
 				
-			} // End woothemes_wp_head()
-		}
-		
-		// Load certain files only in the WordPress admin.
-		if ( is_admin() ) {
-			require_once( $functions_path . 'admin-shortcode-generator.php' ); 	    // Framework Shortcode generator // 2011-01-21.
-		}
+		} // End wfs_wp_head()	
 		
 	} //end class
 	
